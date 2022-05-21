09e41e4c7d15c961388d51c6f7556d85f10b5288
Baek Seungho <seungho1216@naver.com>
2022-05-21 13:29:21 +0900

    first commit

---
+++ src/main/java/com/sparta/neonaduriback/NeonaduriBackApplication.java
@@ -0,0 +1,13 @@
+package com.sparta.neonaduriback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NeonaduriBackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NeonaduriBackApplication.class, args);
+    }
+
+}
