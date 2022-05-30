212637f0c124b02dddeda669cee32f5b47aa8ffd
Baek Seungho <seungho1216@naver.com>
2022-05-31 05:26:17 +0900

    ver6/CiCd 적용 중

--- src/main/java/com/sparta/neonaduriback/config/JasyptConfig.java
+++ src/main/java/com/sparta/neonaduriback/config/JasyptConfig.java
@@ -1,38 +1,38 @@
-//package com.sparta.neonaduriback.config;
-//
-//import org.jasypt.encryption.StringEncryptor;
-//import org.jasypt.encryption.pbe.PooledPBEStringEncryptor;
-//import org.jasypt.encryption.pbe.config.SimpleStringPBEConfig;
-//import org.springframework.beans.factory.annotation.Value;
-//import org.springframework.context.annotation.Bean;
-//import org.springframework.context.annotation.Configuration;
-//
-//@Configuration
-//public class JasyptConfig {
-//
-//    @Value("${jasypt.encryptor.password}")
-//    private String encryptKey;
-//
-//    @Bean("jasyptStringEncryptor")
-//    public StringEncryptor stringEncryptor(){
-//
-//        PooledPBEStringEncryptor encryptor = new PooledPBEStringEncryptor();
-//        SimpleStringPBEConfig config = new SimpleStringPBEConfig();
-//
-////        System.out.println(System.getenv("JASYPT_PASSWORD"));
-////        config.setPassword(System.getenv("JASYPT_PASSWORD"));
-//        config.setPassword(encryptKey);
-//        config.setPoolSize("1");
-//        config.setAlgorithm("PBEWithMD5AndDES");
-//        config.setStringOutputType("base64");
-//        config.setKeyObtentionIterations("1000");
-//        config.setProviderName("SunJCE");
-//        config.setSaltGeneratorClassName("org.jasypt.salt.RandomSaltGenerator");
-//        config.setIvGeneratorClassName("org.jasypt.iv.NoIvGenerator");
-//        //config를 바탕으로 암호화 진행
-//        encryptor.setConfig(config);
-//
-//        //암호화 결과물 return
-//        return encryptor;
-//    }
-//}
+package com.sparta.neonaduriback.config;
+
+import org.jasypt.encryption.StringEncryptor;
+import org.jasypt.encryption.pbe.PooledPBEStringEncryptor;
+import org.jasypt.encryption.pbe.config.SimpleStringPBEConfig;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class JasyptConfig {
+
+    @Value("${jasypt.encryptor.password}")
+    private String encryptKey;
+
+    @Bean("jasyptStringEncryptor")
+    public StringEncryptor stringEncryptor(){
+
+        PooledPBEStringEncryptor encryptor = new PooledPBEStringEncryptor();
+        SimpleStringPBEConfig config = new SimpleStringPBEConfig();
+
+//        System.out.println(System.getenv("JASYPT_PASSWORD"));
+//        config.setPassword(System.getenv("JASYPT_PASSWORD"));
+        config.setPassword(encryptKey);
+        config.setPoolSize("1");
+        config.setAlgorithm("PBEWithMD5AndDES");
+        config.setStringOutputType("base64");
+        config.setKeyObtentionIterations("1000");
+        config.setProviderName("SunJCE");
+        config.setSaltGeneratorClassName("org.jasypt.salt.RandomSaltGenerator");
+        config.setIvGeneratorClassName("org.jasypt.iv.NoIvGenerator");
+        //config를 바탕으로 암호화 진행
+        encryptor.setConfig(config);
+
+        //암호화 결과물 return
+        return encryptor;
+    }
+}
--- src/test/java/com/sparta/neonaduriback/JasyptTest.java
+++ src/test/java/com/sparta/neonaduriback/JasyptTest.java
@@ -1,37 +1,37 @@
-//package com.sparta.neonaduriback;
-//
-//import org.jasypt.encryption.pbe.PooledPBEStringEncryptor;
-//import org.jasypt.encryption.pbe.config.SimpleStringPBEConfig;
-//import org.junit.jupiter.api.Test;
-//
-//import static org.assertj.core.api.Assertions.assertThat;
-//
-//public class JasyptTest {
-//
-//    @Test
-//    public void stringEncryptor(){
-//        String text="평문비밀번호";
-//
-//        PooledPBEStringEncryptor encryptor = new PooledPBEStringEncryptor();
-//        SimpleStringPBEConfig config = new SimpleStringPBEConfig();
-//        System.out.println(System.getenv("JASYPT_PASSWORD"));
-//        config.setPassword(System.getenv("JASYPT_PASSWORD"));
-////        config.setPassword("");
-//        config.setPoolSize("1");
-//        config.setAlgorithm("PBEWithMD5AndDES");
-//        config.setStringOutputType("base64");
-//        config.setKeyObtentionIterations("1000");
-//        config.setProviderName("SunJCE");
-//        config.setSaltGeneratorClassName("org.jasypt.salt.RandomSaltGenerator");
-//        config.setIvGeneratorClassName("org.jasypt.iv.NoIvGenerator");
-//        //config를 바탕으로 암호화 진행
-//        encryptor.setConfig(config);
-//
-//        String encryptText=encryptor.encrypt(text);
-//        System.out.println(encryptText);
-//        String decryptText= encryptor.decrypt(encryptText);
-//        System.out.println(decryptText);
-//        assertThat(text).isEqualTo(decryptText);
-//    }
-//
-//}
+package com.sparta.neonaduriback;
+
+import org.jasypt.encryption.pbe.PooledPBEStringEncryptor;
+import org.jasypt.encryption.pbe.config.SimpleStringPBEConfig;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class JasyptTest {
+
+    @Test
+    public void stringEncryptor(){
+        String text="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";
+
+        PooledPBEStringEncryptor encryptor = new PooledPBEStringEncryptor();
+        SimpleStringPBEConfig config = new SimpleStringPBEConfig();
+        System.out.println(System.getenv("JASYPT_PASSWORD"));
+        config.setPassword(System.getenv("JASYPT_PASSWORD"));
+//        config.setPassword("");
+        config.setPoolSize("1");
+        config.setAlgorithm("PBEWithMD5AndDES");
+        config.setStringOutputType("base64");
+        config.setKeyObtentionIterations("1000");
+        config.setProviderName("SunJCE");
+        config.setSaltGeneratorClassName("org.jasypt.salt.RandomSaltGenerator");
+        config.setIvGeneratorClassName("org.jasypt.iv.NoIvGenerator");
+        //config를 바탕으로 암호화 진행
+        encryptor.setConfig(config);
+
+        String encryptText=encryptor.encrypt(text);
+        System.out.println(encryptText);
+        String decryptText= encryptor.decrypt(encryptText);
+        System.out.println(decryptText);
+        assertThat(text).isEqualTo(decryptText);
+    }
+
+}
